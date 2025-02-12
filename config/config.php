4f685c3f8f86e1e2369ce8874a7221e68c100d7c
dihxrid <hoangdang3210@gmail.com>
2025-02-12 16:29:53 +0700

    update

--- config/config.php
+++ config/config.php
@@ -10,7 +10,7 @@ define("VNPAY_HASH_SECRET", "51CNF74EOXHO7VEELB0W6Z8P6PI8G4MZ");
 define("VNPAY_URL", "https://sandbox.vnpayment.vn/paymentv2/vpcpay.html");
 define("VNPAY_RETURN_URL", "http://localhost:3000/vnpay_return");
 #google login
-// define('CLIENT_ID', '736209475950-s1b94b7kvvcrgjj3cnjavdrupeep304s.apps.googleusercontent.com');
-// define('CLIENT_SECRET', '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');
-// define('REDIRECT_URI', 'http://localhost:3000/login/google/callback');
+define('CLIENT_ID', '');//config google
+define('CLIENT_SECRET', '');
+define('REDIRECT_URI', 'http://localhost:3000/login/google/callback');
 ?>
